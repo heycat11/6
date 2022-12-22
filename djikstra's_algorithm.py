df37ca9df200fd3018d4d124167470913de4826a
heycat11 <121254212+heycat11@users.noreply.github.com>
2022-12-22 23:40:44 +0530

    Add files via upload

---
+++ djikstra's_algorithm.py
@@ -0,0 +1,45 @@
+
+#Djikstras
+import sys
+
+
+class Graph:
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printSolution(self, dist):
+        print("Vertex \tDistance from Source")
+        for node in range(self.V):
+            print(node, "\t", dist[node])
+
+    def minDistance(self, dist, sptSet):
+        min = sys.maxsize
+        for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u
+        return min_index
+
+    def dijkstra(self, src):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+            x = self.minDistance(dist, sptSet)
+            sptSet[x] = True
+            for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
+
+        self.printSolution(dist)
+
+
+g = Graph(6)
+g.graph = [[0, 3, 0, 0, 6, 5], [3, 0, 1, 0, 0, 4], [0, 1, 0, 6, 0, 4],
+           [0, 0, 6, 0, 8, 5], [6, 0, 0, 8, 0, 2], [5, 4, 4, 5, 2, 0]]
+g.dijkstra(0)
